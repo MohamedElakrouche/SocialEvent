2e787c7bf3cab078ac8405d135334eccdd7c242b
Mike Gerlinger <bouarroudjmike@yahoo.fr>
2024-11-13 18:56:54 +0100

    modif_login_and_css

---
+++ socialeventsweb/socialeventlogin.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>Connexion</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="loginstyle.css"> <!-- Link to your CSS file -->
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
+    <title>Login</title>
+   
+<link href="https://fonts.googleapis.com/css2?family=Yellowtail&display=swap" rel="stylesheet">
+    
+<link rel="stylesheet" href="loginstyle.css"> <!-- Link to your CSS file -->
+
+</head>
+<body>
+<img src="Design_sans_titre__5_-removebg-preview.png" alt="socialevents" class="logo">
+    <div class="login-container">
+    <form action="authenticate.php" method="post">
+        <h2>Connexion à mon espace</h2>
+        <p>Entrez vos identifiants pour commencer l'aventure</p>
+                <label for="username">Mail:</label>
+                <input type="text" id="username" name="username" placeholder="Entrez votre mail" required>
+                <label for="password">Password:</label> 
+                <input type="password" id="password" name="password" placeholder="Entrez votre mot de passe"required>
+            <button type="submit">c'est partis</button>
+        </form>
+    </div>
+</body>
+</html>
