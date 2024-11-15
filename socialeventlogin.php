f09226c8f341e1ef1527b815bdd4a4aca62a7357
Mohamed Elakrouche <elakrouche@gmail.com>
2024-11-15 06:00:40 +0100

    test branche

--- socialeventlogin.php
+++ socialeventlogin.php
@@ -12,7 +12,7 @@
    
 <link href="https://fonts.googleapis.com/css2?family=Yellowtail&display=swap" rel="stylesheet">
     
-<link rel="stylesheet" href="css/loginstyle.css"> 
+<link rel="stylesheet" href="css/loginstyle.css "> 
 
 </head>
 <body>
