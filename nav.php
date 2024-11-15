83dc13d79e75ca3e86203cd86366667dccf6f3f8
Mohamed El akrouche <elakrouche@gmail.com>
2024-11-15 16:31:19 +0100

    MAJ du CSS de la NAV

--- nav.php
+++ nav.php
@@ -1,12 +1,18 @@
 <link rel="stylesheet" href="css/nav.css">
 
 <div class="nav">
-    
+
     <ul>
-        <a href="homeEvent.php"> <li>Accueil</li></a>
-        <li>Profil</li>
-        <li>Mes évènements</li>
-        <a href="createEvent.php"><li>Création</li></a>
+        <a href="homeEvent.php">
+            <li>Accueil</li>
+        </a>
+        <a href="#" <li>Profil</li> </a>
+        <a href="#">
+            <li>Mes évènements</li>
+        </a>
+        <a href="createEvent.php">
+            <li>Création</li>
+        </a>
     </ul>
 
 </div>
